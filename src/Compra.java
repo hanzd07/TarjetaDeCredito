4608b0d9039a5f08223b1230f506b5696a92e131
hanz diener <hanz.diener@gmail.com>
2025-07-02 19:47:29 -0500

    Crear una tarjeta de crédito con asignación de saldo deseado
    Se creo un programa sobre una tarjeta de crédito que se la va a imponer cierto saldo limite inicialmente, luego se va a empezar a realizar compras a medida que se hace la compra se guarda esos valor y la descripción de la compra.
    Si se queda sin saldo la tarjeta, el programa va a detener mostrando un resumen de las compras o si se decide parar de comprar también se detiene el programa y se muestra el resumen de las compras y el saldo restante.

---
+++ src/Compra.java
@@ -0,0 +1,33 @@
+
+public class Compra implements Comparable<Compra> {
+	
+	private double valor;
+	private String descripcion;
+	
+	
+	public Compra(double valor, String descripcion) {
+		this.valor = valor;
+		this.descripcion = descripcion;
+	}
+
+
+	public double getValor() {
+		return valor;
+	}
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	@Override
+	public String toString() {
+		return "Compra: valor = " + valor + " Descripcion: " + descripcion;
+	}
+
+
+	@Override
+	public int compareTo(Compra otraCompra) {
+		return Double.valueOf(this.valor).compareTo(Double.valueOf(otraCompra.getValor()));
+	}
+}
